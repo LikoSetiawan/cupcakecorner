343528897ab8168e541f979e6f0194f8eff9f980
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-03-23 15:27:53 +0800

    Initial commit

---
+++ CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Liko Setiawan on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
