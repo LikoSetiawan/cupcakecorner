5386dec266fb2e5cb3c55b83d05e6a0428636ca7
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-03-24 15:51:28 +0800

    creating model

--- ContentView.swift
+++ ContentView.swift
@@ -8,88 +8,44 @@
 import SwiftUI
 
 
-struct Response: Codable{
-    var results : [Result]
-}
-
-struct Result: Codable {
-    var trackId : Int
-    var trackName : String
-    var collectionName : String
-}
-
 struct ContentView: View {
-//    @State private var results = [Result]()
-    
-    @State private var email = ""
-    @State private var username = ""
-    
-    var disableForm: Bool{
-        username.count < 5 || email.count < 5
-    }
+    @State private var order = Order()
     
     var body: some View {
-        Form{
-            Section{
-                TextField("Username", text: $username)
-                TextField("Email", text: $email )
-            }
-            
-            Section{
-                Button("Create Account"){
-                    print("Create Account...")
+        NavigationStack{
+            Form{
+                Section{
+                    Picker("Select your cake type", selection: $order.type){
+                        ForEach(Order.types.indices, id: \.self){ type in
+                            Text(Order.types[type])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                Section{
+                    Toggle("Any special request?", isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled{
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section{
+                    NavigationLink("Delivery Details"){
+                        AddressView(order: order)
+                    }
+                    
                 }
             }
-            .disabled(disableForm)
+            .navigationTitle("Cupcake Corner")
+            
         }
-//        AsyncImage(url: URL(string: "https://hws.dev/img/log.png")) { phase in
-//            if let image = phase.image {
-//                image
-//                    .resizable()
-//                    .scaledToFit()
-//            } else if phase.error != nil {
-//                Text("There was an error loading the image check the URL.")
-//            } else {
-//                ProgressView()
-//            }
-//        }
-//        .frame(width: 200, height: 200)
-//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){image in
-//            image
-//                .resizable()
-//                .scaledToFit()
-//        }placeholder: {
-//            Color.red
-//        }
-//        .frame(width: 200, height: 200)
-//        List(results, id: \.trackId){ item in
-//            VStack(alignment: .leading){
-//                Text(item.trackName)
-//                    .font(.headline)
-//                Text(item.collectionName)
-//            }
-//        }
-//        .task {
-//            await loadData()
-//        }
     }
-    
-    
-//    func loadData() async{
-//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else{
-//            print("Invalid URL")
-//            return
-//        }
-//        do{
-//            let(data, _) = try await URLSession.shared.data(from: url)
-//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
-//                results = decodedResponse.results
-//            }
-//        }catch{
-//            print("Invalid data")
-//        }
-//    }
 }
+
+
 
 #Preview {
     ContentView()
